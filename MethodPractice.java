953b9af2cb30b5534b9ddea4426f8d4f9ab0ff1d
Prince Panjiyar <91174979+Princep02@users.noreply.github.com>
2023-05-26 17:33:25 +0530

    Add files via upload

---
+++ MethodPractice.java
@@ -0,0 +1,39 @@
+package MethodPractice;
+public class MethodPractice{
+//  static int max(int x, int y){
+//     if (x>y)
+//     return x;
+//     else
+//     return y;
+//  }
+//  public static void main(String args[]){
+//     int a=10,b=15;
+//     System.out.println(max(a,b));
+//  }
+
+// int max(int x, int y){
+//    if (x>y)
+//    return x;
+//    else
+//    return y;
+// }
+// public static void main(String args[]){
+//    int a=10,b=15;
+//    MethodPractice mp=new MethodPractice();
+//    System.out.println(mp.max(a,b));
+// }
+
+// static void change(int A[], int index, int value){
+//     A[index]=value;
+// }
+// public static void main(String args[]){
+// int A[]={2,4,5,6,7};
+// change(A,2,8);
+// for(int x:A){
+//     System.out.println(x);
+// }
+// }
+
+
+}
+
